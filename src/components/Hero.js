ea125efaff81a72fae8d9aa9139e7379e87d329e
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-06-02 02:25:35 -0400

    Hero section complete and Dropdown component added to folder structure
    Hero section finished styled images and added animation for hero slider using useEffect, useState and useRef

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { css } from 'styled-components/macro';
 import { Button } from './Button';
 import {
@@ -15,35 +15,79 @@ import {
 } from './Hero.elements';
 
 const Hero = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const timeout = useRef(null);
+
+  //   useEffect(() => {
+  //     const nextSlide = () => {
+  //       setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+  //     };
+
+  //     timeout.current = setTimeout(nextSlide, 3000);
+
+  //     return function () {
+  //       if (timeout.current) {
+  //         clearTimeout(timeout.current);
+  //       }
+  //     };
+  //   }, [current, length]);
+
+  const nextSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    setCurrent(current === length - 1 ? 0 : current + 1);
+
+    // console.log(current);
+  };
+
+  const prevSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    setCurrent(current === 0 ? length - 1 : current - 1);
+
+    // console.log(current);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
     <HeroSection>
       <HeroWrapper>
         {slides.map((slide, index) => {
           return (
             <HeroSlide key={index}>
-              <HeroSlider>
-                <HeroImage src={slide.image} alt={slide.art} />
-                <HeroContent>
-                  <h1>{slide.title}</h1>
-                  <p>{slide.price}</p>
-                  <Button
-                    to={slide.path}
-                    primary='true'
-                    css={`
-                      max-width: 160px;
-                    `}
-                  >
-                    {slide.label}
-                    <Arrow />
-                  </Button>
-                </HeroContent>
-              </HeroSlider>
+              {index === current && (
+                <HeroSlider>
+                  <HeroImage src={slide.image} alt={slide.art} />
+                  <HeroContent>
+                    <h1>{slide.title}</h1>
+                    <p>{slide.price}</p>
+                    <Button
+                      to={slide.path}
+                      primary='true'
+                      css={`
+                        max-width: 160px;
+                      `}
+                    >
+                      {slide.label}
+                      <Arrow />
+                    </Button>
+                  </HeroContent>
+                </HeroSlider>
+              )}
             </HeroSlide>
           );
         })}
         <SliderButtons>
-          <PrevArrow />
-          <NextArrow />
+          <PrevArrow onClick={prevSlide} />
+          <NextArrow onClick={nextSlide} />
         </SliderButtons>
       </HeroWrapper>
     </HeroSection>
